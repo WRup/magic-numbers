d5f1c2e5815981a798201ae4bfc650a5357bc8a1
Wiktor Rup <wiktor-rup@wp.pl>
2019-02-14 22:33:07 +0100

    implemented FileReader mechanism

--- src/com/mnumbers/FileReader.java
+++ src/com/mnumbers/FileReader.java
@@ -1,13 +1,38 @@
 package com.mnumbers;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FileReader {
 
     private File file;
+    private static final int MAX_MAGIC_NUMBERS_POSITION = 13;
 
     private FileReader(String filepath) {
-        this.file = new File(filepath);
+        file = new File(filepath);
+    }
+
+    public List<Integer> readFile() {
+        int currentByte = 0;
+        ArrayList<Integer> fileContent = new ArrayList<>();
+        try (InputStream is = new FileInputStream(file)) {
+            for (int i = 0; i < MAX_MAGIC_NUMBERS_POSITION; i++) {
+                if((currentByte = is.read()) != -1) {
+                    fileContent.add(currentByte);
+                }
+            }
+            return fileContent;
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found!");
+        } catch (IOException e) {
+            System.out.println("Problem with file - cannot be read.");
+        }
+        return fileContent;
     }
 
     static FileReader of(String filepath) {
--- src/com/mnumbers/Main.java
+++ src/com/mnumbers/Main.java
@@ -1,8 +1,14 @@
 package com.mnumbers;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String filepath = "C:\\Users\\Wiktor\\Desktop\\_4273128.jpg";
+        Scanner scanner = new Scanner(System.in);
+        ExtensionValidator extensionValidator = ExtensionValidator.of(filepath);
+        FileReader fileReader = FileReader.of(filepath);
+
     }
 }
